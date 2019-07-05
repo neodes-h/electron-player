d87c093451d7e26a4d0dea364621bb7dc0063d22
Henry <haoyixian1994@hotmail.com>
2019-07-05 15:05:13 -0400

    1. Finished volume slider control.

--- renderer-process/footer.js
+++ renderer-process/footer.js
@@ -10,8 +10,19 @@ let cycle_mode = 0;//0->random,1->by order
 // songSlider.listen('MDCSlider:change', () => console.log(`Value changed to ${songSlider.value}`));
 
 // init slider
-let cur_vol = 20;
-let volumeSliderHandler = ((event) => rootNode.style.setProperty('--volume-slider-left', event.target.value + '%'));
+let cur_vol = 50,mute_flag = false;
+function volumeSliderHandler(event){
+    rootNode.style.setProperty('--volume-slider-left', event.target.value + '%');
+    cur_vol = event.target.value;
+
+    if(cur_vol > 0){
+        document.querySelector('#volume_off').classList.add('remove');
+        document.querySelector('#volume_up').classList.remove('remove');
+    } else {
+        document.querySelector('#volume_off').classList.remove('remove');
+        document.querySelector('#volume_up').classList.add('remove');
+    }
+}
 volumeSlider.setAttribute('value',cur_vol);
 rootNode.style.setProperty('--volume-slider-left', cur_vol + '%');
 // change slider css dynamically
@@ -25,10 +36,30 @@ volumeSliderWrapper.addEventListener('mouseenter',showVolumeSlider);
 volumeSliderWrapper.addEventListener('mouseleave',hideVolumeSlider);
 volumeBtn.addEventListener('mouseenter', showVolumeSlider);
 volumeBtn.addEventListener('mouseleave', hideVolumeSlider);
+volumeBtn.addEventListener('click',(event)=>{
+    if(mute_flag){
+        //unmute
+        mute_flag = false;
+        volumeSlider.value = cur_vol;
+        rootNode.style.setProperty('--volume-slider-left', (cur_vol) + '%');
+        document.querySelector('#volume_off').classList.add('remove');
+        document.querySelector('#volume_up').classList.remove('remove');
+    } else {
+        //mute
+        mute_flag = true;
+        volumeSlider.value = 0;
+        rootNode.style.setProperty('--volume-slider-left', '0%');
+        document.querySelector('#volume_off').classList.remove('remove');
+        document.querySelector('#volume_up').classList.add('remove');
+    }
+})
 
 
 let cur_pos = 0;
-let songSliderHandler = ((event) => rootNode.style.setProperty('--song-slider-left',event.target.value + '%'));
+function songSliderHandler(event){
+    rootNode.style.setProperty('--song-slider-left',event.target.value + '%');
+    cur_pos = event.target.value;
+}
 songSlider.setAttribute('value', cur_pos);
 rootNode.style.setProperty('--song-slider-left', cur_pos + '%');
 // change slider css dynamically
